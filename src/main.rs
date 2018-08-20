4270d08b6b2a3877e2e0910fa89d52aca106babf
dmorigin <stephan@kirfel-koeln.de>
2018-08-20 16:31:28 +0200

    Version

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ mod configuration;
 use configuration::*;
 
 
-const VERSION: &str = "0.1.5";
+const VERSION: &str = "0.2.0";
 const AUTHOR: &str = "[DM]Origin";
 
 // This is the time in seconds in which one iteration
